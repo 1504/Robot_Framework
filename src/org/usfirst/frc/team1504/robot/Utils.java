1626d4c69d1485ff71ca2d7b51683fa63fb60d27
Mike Lownds <cowplex@ananke>
2016-01-20 00:43:46 -0500

    Testing dynamic class loading
    Attempted to set up dynamic class loading to load all classes
    referenced in the LOGGED_CLASSES enum, but would not otherwise be
    loaded. (i.e., the Drive class with no auto or the Lifter class, which
    will never be called by a class that has already been loaded)

--- src/org/usfirst/frc/team1504/robot/Utils.java
+++ src/org/usfirst/frc/team1504/robot/Utils.java
@@ -17,4 +17,23 @@ public class Utils
 			return 0.0;
 		return (input - Map.UTIL_JOYSTICK_DEADZONE * Math.signum(input)) / (1.0 - Map.UTIL_JOYSTICK_DEADZONE);
 	}
+	
+	public static String toCamelCase(final String init)
+	{
+	    if (init==null)
+	        return null;
+
+	    final StringBuilder ret = new StringBuilder(init.length());
+
+	    for (final String word : init.split("_")) {
+	        if (!word.isEmpty()) {
+	            ret.append(word.substring(0, 1).toUpperCase());
+	            ret.append(word.substring(1).toLowerCase());
+	        }
+	        if (!(ret.length()==init.length()))
+	            ret.append("_");
+	    }
+
+	    return ret.toString();
+	}
 }
