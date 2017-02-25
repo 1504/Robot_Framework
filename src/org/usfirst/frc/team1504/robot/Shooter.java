e1d8a48f244882c7443728e84c983514a43bfea2
Lexie Roberts <bestviolingirl@gmail.com>
2017-02-24 20:11:31 -0500

    new shooter!

--- src/org/usfirst/frc/team1504/robot/Shooter.java
+++ src/org/usfirst/frc/team1504/robot/Shooter.java
@@ -1,27 +1,20 @@
 package org.usfirst.frc.team1504.robot;
 
 import org.usfirst.frc.team1504.robot.Update_Semaphore.Updatable;
-
 import com.ctre.CANTalon;
 import com.ctre.CANTalon.FeedbackDevice;
 import com.ctre.CANTalon.TalonControlMode;
 
 import edu.wpi.first.wpilibj.Preferences;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 
 public class Shooter implements Updatable
 {
-	private CANTalon _shooter = new CANTalon(Map.SHOOTER_MOTOR);
-	private CANTalon _helicopter = new CANTalon(Map.HELICOPTER_MOTOR);
-
-	private static final Shooter instance = new Shooter();
-	private static final DriverStation _ds = DriverStation.getInstance();
-	//private static final CameraInterface _camera = CameraInterface.getInstance();
-	private Thread thread;
-
-	private double [][] PID = {{.03, .00015}, {.05, .00017}};
-	private static Preferences _pref = Preferences.getInstance();
+	CANTalon _shooter = new CANTalon(Map.SHOOTER_MOTOR);
+	CANTalon _helicopter = new CANTalon(Map.HELICOPTER_MOTOR);
+	Preferences _pref = Preferences.getInstance();
+	private static Shooter _instance = new Shooter();
 	
 	public Shooter()
 	{
@@ -29,133 +22,67 @@ public class Shooter implements Updatable
 		{
 			_shooter.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
 			_shooter.changeControlMode(TalonControlMode.Speed);
-			_shooter.setP(PID[0][0]);
-			_shooter.setI(PID[0][1]);
+			_shooter.setP(.03);
+			_shooter.setI(.0015);
 			_shooter.reverseSensor(false);
 		}
-		
-		//SmartDashboard.putNumber("Shooter Target Speed", Map.SHOOTER_TARGET_SPEED);
-		//SmartDashboard.putBoolean("Shooter port encoder good", (_sensor_status & 1) != 0);
-		//SmartDashboard.putBoolean("Shooter star encoder good", (_sensor_status & 2) != 0);
-		
 		Update_Semaphore.getInstance().register(this);
-		System.out.println("Shooter Initialized");
+		System.out.println("Shooter initialized to shoot");
 	}
 	
 	public static Shooter getInstance()
 	{
-		return instance;
+		return _instance;
 	}
 	
 	public static void initialize()
 	{
 		getInstance();
 	}
-
+	
+	public void set_target_speed(double speed)
+	{
+		_pref.putDouble("Shooter Target Speed", speed);
+		SmartDashboard.putNumber("Shooter Target Speed", speed);
+	}
+	
 	public boolean getSpeedGood()
 	{
-		return Math.abs(getTargetSpeed() - SmartDashboard.getNumber("Shooter Target Speed", 0.0)) < Map.SHOOTER_PID_DEADZONE; //pref
+		return Math.abs(_shooter.getSpeed() - SmartDashboard.getNumber("Shooter Target Speed", 0.0)) < Map.SHOOTER_PID_DEADZONE;
 	}
 	
-	/*public boolean getSpeedGoodfromCamera()
+	public double get_target_speed()
 	{
-		return Math.abs(getTargetSpeed() - setLinearSpeed(_camera._pipe.getDistance())) < Map.SHOOTER_PID_DEADZONE;
-	}*/
-	
-	public double setLinearSpeed(double distance) //distance in inches for now cuz reasons
-	{
-		return distance * 14.054 + 2980;
+		return _pref.getDouble("Shooter Target Speed", 0.0);
 	}
 	
-	public double setCubicSpeed(double distance) 
+	public void update_dashboard()
 	{
-		double speed = .0096*Math.pow(distance, 3) - 1.8304*Math.pow(distance,  2) + 120.1*distance + 1144.3;
-		
-		System.out.println("Speed is " + speed + "for distance of " + distance);
-
-		return speed;
-	}
-	
-	public double getTargetSpeed()
-	{
-		return -SmartDashboard.getNumber("Shooter target speed", 0.0);
-	}
-	
-	public void setTargetSpeed(double speed)
-	{
-		SmartDashboard.putNumber("Shooter Target Speed", speed);
-		//_pref.putDouble("Shooter Target Speed", speed);
-		
-	}
-	private void update_dashboard()
-	{
-		//Map.SHOOTER_TARGET_SPEED = SmartDashboard.getNumber("Shooter Target Speed");
-//		SmartDashboard.putNumber("Shooter Target Speed", getTargetSpeed());
-		SmartDashboard.putNumber("Shooter Speed", _shooter.getSpeed());
-		//SmartDashboard.putBoolean("Shooter speed good", _speed_good);
-		//SmartDashboard.putString("Shooter State", _state.toString());
-		//SmartDashboard.putBoolean("Shooter Override", _override);
-		
-		//_stopmotion.set_speeds(_shooter_motor_port.getSpeed(), _shooter_motor_star.getSpeed());
+		SmartDashboard.putNumber("Shooter Target Speed", get_target_speed());
 	}
 	
 	public void semaphore_update()
-	{		
-		if(_ds.isEnabled() && IO.shooter_input())
+	{
+		if(IO.shooter_input())
 		{
-			System.out.println("should shoot");
-			_shooter.setP(PID[0][0]);
-			_shooter.setI(PID[0][1]);
-			_shooter.set(-SmartDashboard.getNumber("Shooter Target Speed", 0.0));//getTargetSpeed());
-			
-			if(-SmartDashboard.getNumber("Shooter Target Speed", 0.0) != getTargetSpeed())
-				setTargetSpeed(-SmartDashboard.getNumber("Shooter Target Speed", 0.0));
-			System.out.println("speed is " + -SmartDashboard.getNumber("Shooter Target Speed", 0.0));
-			//_shooter.set(setLinearSpeed(_camera._pipe.getDistance()));
-
-			if(IO.helicopter_pulse())
- 			{
- 				_helicopter.set(1.0);
- 				
- 			}
-			
-			else if(getSpeedGood() || IO.shooter_override())
+			if(get_target_speed() != SmartDashboard.getNumber("Shooter Target Speed", 0.0))
 			{
- 				_helicopter.set(-1.0); 
+				set_target_speed(get_target_speed()); //map the preferences value over to SmartDashboard
 			}
 			
-			else
+			_shooter.set(-get_target_speed());
+			
+			if(getSpeedGood() || IO.shooter_override())
 			{
- 				_helicopter.set(0.0); 
+				_helicopter.set(1.0);
 			}
-		}
-
-		else// if(!IO.shooter_input())
-		{
-			//_shooter.setP(PID[0][0]);
-			//_shooter.setI(PID[0][1]);
-			_shooter.set(0.0);
 		}
 		
-		/*else if(_ds.isEnabled() && IO.camera_shooter_input()) //|| _camera._isAimed)
+		else
 		{
-			if(_camera._isAimed)
-			{
-				_shooter.setP(PID[0][0]);
-				_shooter.setI(PID[0][1]);
-				_shooter.set(_camera.get_shooter_speed());
-			}
-			else
-			{
-				_shooter.setP(PID[1][0]);
-				_shooter.setI(PID[1][1]);
-				_shooter.set(_camera.get_shooter_speed());
-			}
-			_camera.set_drive_input(); //align to target
-		}*/	
-		//update_dashboard();
+			_shooter.set(0.0);
+			_helicopter.set(0.0);
+		}
+		update_dashboard();
 	}
-	
-
-	
 }
