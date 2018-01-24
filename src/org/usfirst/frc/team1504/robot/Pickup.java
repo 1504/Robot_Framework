ea1ff3627168994fa0e71004bbaf85723d2ac0a5
Jayden Elliott <jdelliott2003@gmail.com>
2018-01-24 16:26:42 -0500

    Added _ to arm_right and arm_left in pickup

--- src/org/usfirst/frc/team1504/robot/Pickup.java
+++ src/org/usfirst/frc/team1504/robot/Pickup.java
@@ -8,8 +8,8 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 public class Pickup implements Updatable {
 	private WPI_TalonSRX _grab_left;
 	private WPI_TalonSRX _grab_right;
-	private WPI_TalonSRX arm_left;
-	private WPI_TalonSRX arm_right;
+	private WPI_TalonSRX _arm_left;
+	private WPI_TalonSRX _arm_right;
 	DoubleSolenoid _grab_piston; 
 	private Lift _lift = Lift.getInstance();
 	private enum state {OFF, ON};
@@ -31,15 +31,15 @@ public class Pickup implements Updatable {
 	{	
 		_grab_left = new WPI_TalonSRX(Map.PICKUP_TALON_PORT_LEFT);
 		_grab_right = new WPI_TalonSRX(Map.PICKUP_TALON_PORT_RIGHT);
-		arm_left = new WPI_TalonSRX(Map.DROP_PICKUP_LEFT);
-		arm_right = new WPI_TalonSRX(Map.DROP_PICKUP_RIGHT);
-		arm_right.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 200); //200 here is the ms timeout when trying to connect
-		arm_right.config_kP(0, 0.03, 200); //200 is the timeout ms
-		arm_right.config_kI(0, 0.00015, 200);
-		arm_left.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 200); //200 here is the ms timeout when trying to connect
-		arm_left.config_kP(0, 0.03, 200); //200 is the timeout ms
-		arm_left.config_kI(0, 0.00015, 200);
-		//arm_left.set(ControlMode.Velocity, 0);
+		_arm_left = new WPI_TalonSRX(Map.DROP_PICKUP_LEFT);
+		_arm_right = new WPI_TalonSRX(Map.DROP_PICKUP_RIGHT);
+		_arm_right.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 200); //200 here is the ms timeout when trying to connect
+		_arm_right.config_kP(0, 0.03, 200); //200 is the timeout ms
+		_arm_right.config_kI(0, 0.00015, 200);
+		_arm_left.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 200); //200 here is the ms timeout when trying to connect
+		_arm_left.config_kP(0, 0.03, 200); //200 is the timeout ms
+		_arm_left.config_kI(0, 0.00015, 200);
+		//_arm_left.set(ControlMode.Velocity, 0);
 		_grab_piston = new DoubleSolenoid(0, 1); //0 is on/forward, 1 for off/reverse
 		_grab_piston.set(DoubleSolenoid.Value.kOff); //not sure about this
 		Update_Semaphore.getInstance().register(this);
@@ -60,13 +60,13 @@ public class Pickup implements Updatable {
 	
 	public void set_arm_speed(double speed) 
 	{
-		arm_left.set(ControlMode.Velocity, speed);
-		arm_right.set(ControlMode.Velocity, speed);
+		_arm_left.set(ControlMode.Velocity, speed);
+		_arm_right.set(ControlMode.Velocity, speed);
 	}
 	
 	public void pick_up()
 	{
-		if(arm_left.getSelectedSensorPosition(0) > 1000 && _lift.pickup_safe()){ //1000 is a constant going up is higher
+		if(_arm_left.getSelectedSensorPosition(0) > 1000 && _lift.pickup_safe()){ //1000 is a constant going up is higher
 			set_arm_speed(0.3);
 		} else{
 			set_arm_speed(0);
@@ -76,7 +76,7 @@ public class Pickup implements Updatable {
 	
 	public void put_down()
 	{
-		if(arm_left.getSelectedSensorPosition(0) < 1000){ //1000 is a constant
+		if(_arm_left.getSelectedSensorPosition(0) < 1000){ //1000 is a constant
 			set_arm_speed(-0.3);
 		} else{
 			set_arm_speed(0);
@@ -150,8 +150,8 @@ public class Pickup implements Updatable {
 	{
 		if (IO.get_override_pickup())
 		{
-			arm_left.set(ControlMode.Velocity, IO.intake_input()*Map.PICKUP_LEFT_MAGIC);
-			arm_right.set(ControlMode.Velocity, IO.intake_input()*Map.PICKUP_RIGHT_MAGIC);
+			_arm_left.set(ControlMode.Velocity, IO.intake_input()*Map.PICKUP_LEFT_MAGIC);
+			_arm_right.set(ControlMode.Velocity, IO.intake_input()*Map.PICKUP_RIGHT_MAGIC);
 		}
 	}
 	public void semaphore_update()
