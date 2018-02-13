e3daa29f2bb835eaa064adbbc3da6c9d27326ac0
Ajusa <arhamjain@gmail.com>
2018-02-12 19:03:03 -0500

    added back in winch stuff on the map

--- src/org/usfirst/frc/team1504/robot/Map.java
+++ src/org/usfirst/frc/team1504/robot/Map.java
@@ -64,11 +64,23 @@ public class Map {
 		public static final double LIFT_MIN_HEIGHT = 0; 
 		
 		public static final double LIFT_GAIN = 0.3;
+		public static final double LIFT_SAFETY_THRESHOLD = 5;
 /*
  * Winch Stuff
  */
 		
 		public static final int INTAKE_POWER_AXIS = 1;
+		public static final int NANCY_TALON_PORT = 40;
+		public static final int MEAD_TALON_PORT = 41;
+		
+		public static final int WINCH_CURRENT_LIMIT = 70;
+		
+		public static final int WINCH_POWER_AXIS = 1;
+		
+		public static final double WINCH_DIRECTION = 1.0;
+		public static final double WINCH_SERVO_DEPLOYED = 180.0;
+		public static final double WINCH_SERVO_STORED = 0.0;
+		public static final double WINCH_BRAKE_TIMEOUT = 15.0;
 		
 /**
  * Drive class things
