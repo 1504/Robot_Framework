ce19283e1fe2a405862e5e2ad0d7d1e18967c77f
Ajusa <arhamjain@gmail.com>
2018-03-22 19:58:47 -0400

    reverted to ultrasonic crash detection
    untested code oh boy
    
    i hope this lin reg works

--- src/org/usfirst/frc/team1504/robot/Drive.java
+++ src/org/usfirst/frc/team1504/robot/Drive.java
@@ -399,7 +399,7 @@ public class Drive implements Updatable
 	double initialSpike = 0.0;
 	double highestTravelingSpike = 0.0;
 	double accelSign = -1.0;
-	public double[] roborio_crash_bandicoot_check(double[] input, long time, int type) {//uses roborio built in accelerometer
+	/*public double[] roborio_crash_bandicoot_check(double[] input, long time, int type) {//uses roborio built in accelerometer
 		double[] null_response = {0.0, 0.0, 0.0, 0, 0};
 		double robot_accel = 0;
 		if(type == 1)
@@ -449,8 +449,8 @@ public class Drive implements Updatable
 			initialSpike = robot_accel*accelSign;
 		}
 		return input;
-	}
-	/*public double[] roborio_crash_bandicoot_check(double[] input, long time) {
+	}*/
+	public double[] roborio_crash_bandicoot_check(double[] input, long time, int mode) {
 		double[] null_response = {0.0, 0.0, 0.0, 0, 0};
 		autonDistances.add(sanic_value());
 		autonTimes.add(time);		
@@ -470,7 +470,7 @@ public class Drive implements Updatable
 			return null_response;
 		}
 		return input;
-	}*/
+	}
 	public void spike_reset() {
 		initialSpike = 0.0;
 		highestTravelingSpike = 0.0;
