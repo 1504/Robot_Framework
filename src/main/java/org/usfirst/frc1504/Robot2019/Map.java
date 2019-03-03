d49db917eb6d3af5576b650dfd16edc388fa3136
Sci-Phy <tristanlusardi@gmail.com>
2019-03-03 11:47:38 -0500

    Add elevator multiplier
    Added Elevator multiplier in Map.java
    Rename first and second linear actuators to bottom and top respectively

--- src/main/java/org/usfirst/frc1504/Robot2019/Map.java
+++ src/main/java/org/usfirst/frc1504/Robot2019/Map.java
@@ -127,20 +127,23 @@ public class Map {
 		 */
 
 		/**
-		 * Arm Delivery Settings for Hatch Panels
+		 * Elevator Delivery Settings for Hatch Panels
 		 * 
 		 * Linear Actuator = {Home, Low, Medium, High}
 		 */
-		public static final double[] first_pm_hatch_levels = {18.4, 10.6, 10.6, 80.0};
-		public static final double[] second_pm_hatch_levels = {12.0, 24.5, 72.4, 71.0};
+		public static final double[] BOTTON_PM_HATCH_LEVELS = {18.4, 10.6, 10.6, 80.0};
+		public static final double[] TOP_PM_HATCH_LEVELS = {12.0, 24.5, 72.4, 71.0};
 
 		/**
-		 * Arm Delivery Settings for Balls
+		 * Elevator Delivery Settings for Balls
 		 * 
 		 * Linear Actuator = {Home, Low, Medium, High, Human Pickup}
 		 */
-		public static final double[] first_pm_ball_levels = {10.4, 12.5, 30.9, 74.3, 16.1};
-		public static final double[] second_pm_ball_levels = {13.8, 40.3, 63.9, 72.5, 58.8};
+		public static final double[] BOTTOM_PM_BALL_LEVELS = {10.4, 12.5, 30.9, 74.3, 16.1};
+		public static final double[] TOP_PM_BALL_LEVELS = {13.8, 40.3, 63.9, 72.5, 58.8};
+
+		// Elevator 
+		public static final double ELEVATOR_SPEED_MAGIC_NUMBER = 0.2;
 
 
 		
@@ -226,7 +229,7 @@ public class Map {
 	/**
 	 * Gear stuff
 	 */
-	public static final double GEAR_DISTANCE = .096;
+	public static final double GEAR_DISTANCE = .0ele6;
 	public static final double GEAR_GAIN = .75;
 	public static final double GEAR_MAX_OUTPUT_POWER = .25;
 
